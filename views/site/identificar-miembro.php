df176f73ee20e581421ed0529d46c8c3a87e1d22
Humberto Antonio Marquez <humberto@2gom.com.mx>
2017-10-23 20:24:27 -0500

    Agregado modal

--- views/site/identificar-miembro.php
+++ views/site/identificar-miembro.php
@@ -41,3 +41,22 @@ $this->registerJsFile(
     </div>
 </div>
    
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Resultado</h4>
+                </div>
+                <div class="modal-body">
+
+                <h5>Hola <span  id="nombre-usuario"></span></h5>
+                    <img style="width:100%;" id="imagen-encontrada" />
+                </div>
+                
+                </div>
+            </div>
+        </div>
