9dc5c31d584c6e01e9ce3b969af530b4de80efb0
Humberto Antonio Marquez <humberto@2gom.com.mx>
2017-10-24 11:15:16 -0500

    Correcciones

--- web/webAssets/js/identificar-usuario.js
+++ web/webAssets/js/identificar-usuario.js
@@ -80,9 +80,8 @@ $(document).ready(function () {
         e.preventDefault();
         var token = $(this).data("token");
         var url = baseUrl+"site/agregar-entrada";
-        var formEntrada = $("#form-agregar-registro");
-        var data = formEntrada.serializeArray();
-        data.push({token:token});
+        var formEntrada = $("#form-agregar-registro")+ "&token=" + token;
+        
         $.ajax({
             url: url,
             type: "POST",
